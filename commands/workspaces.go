67b95498357c56ccf566c1885a910572ae79fef2
Ryan Schmukler <rschmukler@gmail.com>
2015-11-10 21:58:52 -0500

    add workspace deletion and listing of defaults

--- commands/workspaces.go
+++ commands/workspaces.go
@@ -19,11 +19,17 @@ func WorkspaceAdd(ctx *appCtx) error {
 
 func WorkspaceList(ctx *appCtx) error {
 	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"Name"})
+	table.SetHeader([]string{"Name", "Default"})
 	table.SetBorder(false)
 
 	for _, workspace := range ctx.app.Workspaces {
-		table.Append([]string{workspace})
+		var isDefault string
+		if ctx.app.DefaultWorkspace == workspace {
+			isDefault = "*"
+		} else {
+			isDefault = ""
+		}
+		table.Append([]string{workspace, isDefault})
 	}
 
 	table.Render()
@@ -31,6 +37,13 @@ func WorkspaceList(ctx *appCtx) error {
 }
 
 func WorkspaceRemove(ctx *appCtx) error {
+	name := ctx.cli.Args().First()
+	if err := ctx.app.RemoveWorkspace(name); err != nil {
+		return err
+	}
+	if err := ctx.app.Save(); err != nil {
+		return err
+	}
 	return nil
 }
 
